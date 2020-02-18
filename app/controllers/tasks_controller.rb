d2562fe25454ba2b00ea231f6305d4d1e5bba278
Juan Pablo Aponte <juan8419@gmail.com>
2020-02-18 13:26:08 +0000

    adding default value to complted

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,23 @@
 class TasksController < ApplicationController
   def index
     @tasks = Task.all
+  end
 
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+
+    # no need for app/views/restaurants/create.html.erb
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
